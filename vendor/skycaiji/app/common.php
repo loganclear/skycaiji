2ed37e2976a51a4909d107c70f6c296ce2e0c4ac
蓝天采集器 <285909184@qq.com>
2026-09-03 13:08:05 +0800

    3.0.9

--- vendor/skycaiji/app/common.php
+++ vendor/skycaiji/app/common.php
@@ -10,7 +10,7 @@
  */
 
 
-define('SKYCAIJI_VERSION', '3.0.8');
+define('SKYCAIJI_VERSION', '3.0.9');
 \think\Loader::addNamespace('plugin',SKYCAIJI_PATH.'plugin');
 \think\Loader::addNamespace('util',APP_PATH.'extend/util');
 
